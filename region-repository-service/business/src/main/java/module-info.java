c8535eb1c93e175533b5913846e99575dd1cf9fa
Andrei Stelea <stelea.andrei@gmail.com>
2020-04-21 21:00:39 +0300

    Fix gitignore

---
+++ region-repository-service/business/src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module regionrepositoryservice.business {
+	requires spring.boot;
+	requires spring.boot.autoconfigure;
+	
+	exports com.astl.esk.ms.regionrepositoryservice.config;
+}
---
+++ region-repository-service/lib/src/main/java/module-info.java
@@ -0,0 +1,3 @@
+module regionrepositoryservice.lib {
+	
+}
